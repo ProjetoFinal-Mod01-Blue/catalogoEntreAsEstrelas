bc9c20fb887a94ebcfcb5e402b402704a1af452f
anamsilva1981 <78694472+anamsilva1981@users.noreply.github.com>
2021-08-11 20:41:10 -0300

    Add files via upload

---
+++ catalogoEntreAsEstrelas-main/app.py
@@ -0,0 +1,9 @@
+
+from flask import Flask, render_template, redirect, request, session, flash
+from flask_mail import Mail, Message #Importa o Mail e o Message do flask_mail para facilitar o envio de emails
+from flask_sqlalchemy import SQLAlchemy # ORM responsável por realizar as operações do banco de dados via Python
+# from mail_config import email, mail_senha # Módulo para esconder meu user e senha do email.
+
+app = Flask(__name__)
+app.secret_key = 'bluedtech'
+
